add2d24c762ae7eb9c3a09a552ba63597c65ab56
amine <minabd112022@gmail.com>
2026-05-10 16:55:21 +0100

    fix clerk middleware auth issue

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/pricing",
+  "/api/visitors",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/api/(.*)",
+  ],
+};
